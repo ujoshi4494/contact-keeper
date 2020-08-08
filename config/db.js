2a2cb12e8990fcf8212ed8b881f2ec13f7cc779b
umang.j1 <umang.j@peopleinteractive.in>
2020-08-08 18:33:46 +0530

    ready for deploy

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    const result = await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    });
+    console.log("Database Connected");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 const connectDB = require("./config/db");
 
@@ -12,6 +12,14 @@ app.use(express.json({ extended: false }));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/auth", require("./routes/auth"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const port = process.env.PORT || 5000;
 
